f8b05ca53996168a02ee7fc9bef56402038879a7
pecee06 <priyanshu.choudhury2406@outlook.com>
2026-07-04 04:10:46 +0530

    fix bug of incorrect order for sending status code and removed unnecessary code for sending 200 status code

--- src/routes/calculator.js
+++ src/routes/calculator.js
@@ -8,50 +8,50 @@ const calc = new ScientificCalculator();
 router.post("/add", (req, res) => {
 	const { a, b } = req.body;
 
-	res.send(calc.add(Number(a), Number(b))).status(200);
+	res.send(calc.add(Number(a), Number(b)));
 });
 
 router.post("/sub", (req, res) => {
 	const { a, b } = req.body;
 
-	res.send(calc.subtract(Number(a), Number(b))).status(200);
+	res.send(calc.subtract(Number(a), Number(b)));
 });
 
 router.post("/mul", (req, res) => {
 	const { a, b } = req.body;
 
-	res.send(calc.multiply(Number(a), Number(b))).status(200);
+	res.send(calc.multiply(Number(a), Number(b)));
 });
 
 router.post("/div", (req, res) => {
 	const { a, b } = req.body;
 
 	try {
-		res.send(calc.divide(Number(a), Number(b))).status(200);
+		res.send(calc.divide(Number(a), Number(b)));
 	} catch (error) {
-		res.send(error.message).status(400);
+		res.status(400).send(error.message);
 	}
 });
 
 router.post("/sin", (req, res) => {
 	const { a } = req.body;
 
-	res.send(calc.sin(Number(a))).status(200);
+	res.send(calc.sin(Number(a)));
 });
 
 router.post("/cos", (req, res) => {
 	const { a } = req.body;
 
-	res.send(calc.cos(Number(a))).status(200);
+	res.send(calc.cos(Number(a)));
 });
 
 router.post("/tan", (req, res) => {
 	const { a } = req.body;
 
 	try {
-		res.send(calc.tan(Number(a))).status(200);
+		res.send(calc.tan(Number(a)));
 	} catch (error) {
-		res.send(error.message).status(400);
+		res.status(400).send(error.message);
 	}
 });
 
@@ -59,9 +59,9 @@ router.post("/lon", (req, res) => {
 	const { a } = req.body;
 
 	try {
-		res.send(calc.ln(Number(a))).status(200);
+		res.send(calc.ln(Number(a)));
 	} catch (error) {
-		res.send(error.message).status(400);
+		res.status(400).send(error.message);
 	}
 });
 
