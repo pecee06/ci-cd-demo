d4ffa0ca33fcfd7cd7e66f1464005c09493b6f41
pecee06 <priyanshu.choudhury2406@outlook.com>
2026-07-04 02:43:18 +0530

    make a listening server with boilerplate code

---
+++ src/server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(
+	express.urlencoded({
+		extended: true
+	})
+);
+
+app.listen(process.env.PORT, () => {
+	console.log(`Server listening at http://localhost:${process.env.PORT}`);
+});
